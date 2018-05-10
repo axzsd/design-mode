3fabd0a99ee807a7f1d2cf486e20f4951c279a3b
axzsd <atva725@qq.com>
2018-05-10 15:24:24 +0800

    wl 提交readme

--- src/main/java/com/wl/creational/simplefactorypattern/Main.java
+++ src/main/java/com/wl/creational/simplefactorypattern/Main.java
@@ -3,9 +3,12 @@ package com.wl.creational.simplefactorypattern;
 import com.wl.creational.simplefactorypattern.factory.GeometrySimpleFactory;
 import com.wl.creational.simplefactorypattern.framework.Geometry;
 
+/**
+ * 说明：简单工厂模式
+ * */
 public class Main {
     public static void main(String[] args) {
-        Geometry geometry = GeometrySimpleFactory.geometryFactory("yzx");
+        Geometry geometry = GeometrySimpleFactory.geometryFactory("sjx");
         geometry.draw();
         geometry.erase();
     }
