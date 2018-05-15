ec13afefd1355ec6c3ec9ba93149db1a9fac309c
axzsd <atva725@qq.com>
2018-05-15 16:15:04 +0800

    wl 提交外观模式

--- src/main/java/com/wl/structure/facade/Main.java
+++ src/main/java/com/wl/structure/facade/Main.java
@@ -5,6 +5,7 @@ public class Main {
         EncryptFacade encryptFacade = new EncryptFacade("D:/test.txt", "D:/target.txt");
         encryptFacade.convertor();
 
+        // 注意，并没有把加密后的解码
         encryptFacade = new EncryptFacade("D:/target.txt", "D:/target01.txt");
         encryptFacade.reverseConvertor();
     }
